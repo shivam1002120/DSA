a30ebfa4d56a2f4116fa4caf2ab2d35f4cf8e676
ShivamPratap <shivam.pratap@unlink-technologies.com>
2025-10-15 02:12:07 +0530

    new folders l1 and l2

---
+++ 00Leetcode/0001twosum.java
@@ -0,0 +1,50 @@
+//simple brute force approach
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // If no valid pair is found, return an empty array instead of null
+        return new int[] {};
+    }
+}
+
+// using map, in one go we are storing elements in map
+// and other go we are checking for complement
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // In case there is no solution, return an empty array
+        return new int[] {};
+    }
+}
+
+//using map and finding solution in one pass
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // Return an empty array if no solution is found
+        return new int[] {};
+    }
+}
